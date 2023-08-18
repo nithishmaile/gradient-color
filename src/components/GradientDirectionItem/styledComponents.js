82952f08be83bf9ec79f3b41fe8b0ba9f774fad1
nithishkumar <nithishmaile@gmail.com>
2023-08-18 07:54:17 +0000

    2 test cases failed

--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -12,7 +12,7 @@ export const ListItem = styled.li`
 export const DirectionButton = styled.button`
   text-align: center;
   color: ${props => (props.isActive ? '#334155' : '#1e293b')};
-  background-color"#ffffff;
+  background-color:"#ffffff;
   font-family:"Roboto";
   font-size:14px;
   font-weight:600;
